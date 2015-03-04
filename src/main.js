f28377c10eac63106eeda7f973c2c4076602b147
MattiasStenmark <Mattias@Mattiass-MacBook-Pro.local>
2015-03-04 22:28:38 +0100

    mousemove-hit when mouse is over a creature

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 var world = new worldObject();
 var g = new graphics();
 
-var canvasHovering = function(){
-	var a = 1;
+var canvasHovering = function(e){
+	var creatures = world.getCreatures();
+	for(var idx=0;idx<creatures.length;idx++){
+		var pos = creatures[idx].getCurrentPosition();
+		if(Math.abs(e.clientX - pos.x) > 3){
+			continue;
+		}
+		if(Math.abs(e.clientY - pos.y) > 3){
+			continue;
+		}
+
+		console.log(e.clientX + ','+ e.clientY);
+	}
 }
 
 var writeWorld = function(size){
 	var canvas = "<canvas id='myCanvas' width='"+size.maxX*2+"' height='"+size.maxY*2+"' "+
-	"style='border:1px solid #d3d3d3;' onmousemove='canvasHovering()''>"+
+	"style='border:1px solid #d3d3d3;' onmousemove='canvasHovering(event)''>"+
     "Your browser does not support the HTML5 canvas tag.</canvas>";
 	document.write(canvas);
 
